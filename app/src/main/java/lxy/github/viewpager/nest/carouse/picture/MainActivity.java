0d5f456e864a61acf14012061636b0ceb5074a7f
liuxiaoyu <lxynnx@163.com>
2016-07-16 13:36:22 +0800

    viewpager

--- app/src/main/java/lxy/github/viewpager/nest/carouse/picture/MainActivity.java
+++ app/src/main/java/lxy/github/viewpager/nest/carouse/picture/MainActivity.java
@@ -1,13 +1,49 @@
 package lxy.github.viewpager.nest.carouse.picture;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mViewpager;
+    private HashMap<Integer, Fragment> fragments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        fragments = new HashMap<>(3);
+        fragments.put(0, new FirstFrag());
+        fragments.put(1, new SecondFrag());
+        fragments.put(2, new ThirdFrag());
+
+        mViewpager = (ViewPager) findViewById(R.id.viewpager);
+        mViewpager.setAdapter(new MyAdapter(getSupportFragmentManager()));
+    }
+
+    class MyAdapter extends FragmentPagerAdapter {
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
     }
 }
